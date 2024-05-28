992b131248d757367c30ae91f26e358ad95dc6a9
DuongVanTuan <duong.van.tuan@uruca.jp>
2024-05-28 14:07:58 +0700

    fix default index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/user/Login';
-import reportWebVitals from './reportWebVitals';
-import Home from './components/user/home/Home'
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      {/* Route cho trang đăng nhập */}
-      <Route path="/login" element={<Login />} />
-      {/* Route cho ứng dụng chính */}
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
